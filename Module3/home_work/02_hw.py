c13a1b8daf3a90f32444c53753421947b9c45043
AleksandrinaSmirnova <106873962+AlexiyaS@users.noreply.github.com>
2022-06-15 12:15:17 +0300

    Update 02_hw.py

--- Module3/home_work/02_hw.py
+++ Module3/home_work/02_hw.py
@@ -4,5 +4,17 @@
 # для получения случайного числа используйте функцию randint() модуля random
 import random
 numbers = []
+
 # print(random.randint(10, 20))  # раскомментируйте, чтобы посмотреть работу функции randint
 # TODO: your code here
+
+import random
+n = int(input("Введите число элементов: "))
+numbers = []
+
+i = 0
+while i < n:
+    element = random.randint(-100, 100)
+    numbers.insert(i, element)
+    i += 1
+print(numbers)
