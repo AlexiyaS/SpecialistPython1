d4d87574091e72d63ababcc4f58bb4c27d9abb68
AleksandrinaSmirnova <106873962+AlexiyaS@users.noreply.github.com>
2022-06-18 12:48:13 +0300

    Update 04_task_list.py

--- Module3/practice/list_and_tuple/04_task_list.py
+++ Module3/practice/list_and_tuple/04_task_list.py
@@ -1,4 +1,15 @@
 # Дан список из n элементов, заполненный произвольными целыми числами в диапазоне от -10 до 10.
 # Вывести на экран сумму всех положительных элементов.
 
-# TODO: your code here
+import random
+numbers = []
+n = int(input("Введите число элементов: "))
+for _ in range(n):
+    number = random.randint(-10, 10)
+    numbers.append(number)
+print(numbers)
+summa = 0
+for number in numbers:
+    if number > 0:
+        summa += number
+print(summa)
